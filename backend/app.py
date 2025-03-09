d7e19103c2ba85d3e980eda7e5db3b5ed43ad85f
Andrew Knight <andrew.knight14wfc@gmail.com>
2025-03-09 12:49:59 +0000

    Designed database schema and table, and populated database

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,28 @@
+import os
 from flask import Flask
+import sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# Get the database URL from environment variables
+DATABASE_URL = os.getenv("DATABASE_URL", "mysql://root:rootpassword@db:3308/musicdb")
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+db = SQLAlchemy(app)
+
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    artist_name = db.Column(db.String(255), nullable=False)
+    genre = db.Column(db.String(255), nullable=False)
+    song = db.Column(db.String(255), nullable=False)
+    bpm = db.Column(db.Integer, nullable=False)
+    topic = db.Column(db.String(255), nullable=False)
+
+@app.route('/')
+def index():
+    songs = Song.query.all()
+    return str(songs)
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
